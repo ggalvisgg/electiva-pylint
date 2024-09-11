a3e0f48f1dac2fbc337b954e405fdc758b46d31b
ggalvisgg <gabrielagalvisgg@gmail.com>
2024-09-10 20:40:02 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+
+Esta clase suma
+
+"""
+
+def suma(num1, num2):
+
+    """
+    Calculate the sum of two numbers
+
+    Args:
+        num1 (int or float): The first number.
+        num2 (int or float): The second number.
+
+    Returns:
+    int or float: THe sum of num1 and num2.
+    """
+
+    return num1 + num2
